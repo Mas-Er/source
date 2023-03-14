b4ea1fbc5ef667be55cad7786834b23a5e7cfc6f
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2023-03-14 08:42:38 +0700

    Update target.mk

--- target/linux/rockchip/armv7/target.mk
+++ target/linux/rockchip/armv7/target.mk
@@ -4,6 +4,6 @@ CPU_TYPE:=cortex-a7
 BOARDNAME:=RK322x boards (32 bit)
 
 define Target/Description
-	Build firmware image for Rockchip RK33xx devices.
+	Build firmware image for Rockchip RK322x devices.
 	This firmware features a 32 bit kernel.
 endef
